7f906d2b5753d6c9f3487223a7c3c97cc9aaa191
Eren Temel <erenray@gmail.com>
2026-07-19 02:08:38 +0300

    fix: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://vonostudio.com',
+  site: 'https://TemelEren.github.io',
+  base: '/vonostudio.com',
   i18n: {
     defaultLocale: 'tr',
     locales: ['tr', 'en'],
     routing: {
       prefixDefaultLocale: false,
     },
-  },
+  }
 });
